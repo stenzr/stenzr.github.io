ac3aeb690e65710fd98b808afe46e06e7b3589e2
stenzr <rkrohitkr01@gmail.com>
2024-07-15 08:09:14 +0530

    fix

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 import { populateHeadSection, fetchProfileData, populateAboutSection, fetchMediumPosts, fetchTimelineData, populateSocialIcons, fetchNavBarData } from './utilities.js';
 
 // ensure that js is called after the dom is fully loaded
-document.addEventListener("DOMContentLoaded", () => {
-  populateHeadSection();
-  fetchProfileData();
-  populateAboutSection();
-  fetchNavBarData();
-  populateSocialIcons();
-  fetchTimelineData();
-  fetchMediumPosts();
-});
+populateHeadSection();
+fetchProfileData();
+populateAboutSection();
+fetchNavBarData();
+populateSocialIcons();
+fetchTimelineData();
+fetchMediumPosts();
