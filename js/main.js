873c7b3a3078b77a5682833fde969c961b7a9dd4
stenzr <rkrohitkr01@gmail.com>
2024-07-15 08:05:23 +0530

    defer the load of js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 import { populateHeadSection, fetchProfileData, populateAboutSection, fetchMediumPosts, fetchTimelineData, populateSocialIcons, fetchNavBarData } from './utilities.js';
 
-populateHeadSection();
-fetchProfileData();
-populateAboutSection();
-fetchNavBarData();
-populateSocialIcons();
-fetchTimelineData();
-fetchMediumPosts();
-
-  
+// ensure that js is called after the dom is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  populateHeadSection();
+  fetchProfileData();
+  populateAboutSection();
+  fetchNavBarData();
+  populateSocialIcons();
+  fetchTimelineData();
+  fetchMediumPosts();
+});
